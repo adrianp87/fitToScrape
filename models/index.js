5618d7b89f9d4a400a82acec0b69ee2d5e0646c5
Adrian Perez <adrian.perez87@gmail.com>
2019-07-24 11:46:16 -0700

    added code to some files and setup plate

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const { Schema, model } = require('mongoose')
+
+const db = {
+  
+}
+
+module.exports = db
---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+require('./routes')(app)
+
+const port = process.env.PORT || 3000
+
+if (port === 3000) {
+    var MONGODB_URI = `mongodb://localhost/${process.env.MONGO_DB}`
+} else {
+    var MONGODB_URI = process.env.MONGODB_URI
+}
+
+require('mongoose').connect(MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
+  .then(_ => app.listen(port, () => console.log(`server running on port ${port}`)))
+  .catch(e => console.log(e))
